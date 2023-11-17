750c1c784276d8e351a6b7ba413351cff6ccfec1
Diego <tpddiego@gmail.com>
2023-11-17 09:23:58 -0300

    project class, tests class and files configurations

---
+++ src/main/java/br/gov/com/diegofreitas/desafiopicpay/DesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package br.gov.com.diegofreitas.desafiopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPicpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/gov/com/diegofreitas/desafiopicpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.com.diegofreitas.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
